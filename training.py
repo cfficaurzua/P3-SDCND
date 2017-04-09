dbd03d7ab9629f6c905a374cf053a13a58a108bb
Cristobal Fica <Cristobal Fica>
2017-04-09 11:16:11 -0300

    lenet + flipped images, working

--- training.py
+++ training.py
@@ -2,8 +2,7 @@ import csv
 import cv2
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Dropout
-from keras.layers.convolutional import Convolution2D
+from keras.layers import Flatten, Dense, Lambda, Dropout, Conv2D
 from keras.layers.pooling import MaxPooling2D
 lines = []
 with open('./data/driving_log.csv') as csvfile:
@@ -18,22 +17,27 @@ for line in lines:
     filename = source_path.split('\\')[-1]
     current_path = './data/IMG/' + filename
     image = cv2.imread(current_path)
+    flipped_image = cv2.flip(image,1)
+    measurement = float(line[3])
+    flipped_measurement = -1.0 * measurement
     images.append(image)
-    measurements.append(float(line[3]))
+    measurements.append(measurement)
+    images.append(flipped_image)
+    measurements.append(flipped_measurement)
 
 X_train = np.array(images)
 y_train = np.array(measurements)
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape = (160, 320, 3)))
-model.add(Convolution2D(6,5,5, activation = 'relu'))
-model.add(MaxPooling2D(pool_size=2,2))
-model.add(Convolution2D(6,5,5, activation = 'relu'))
-model.add(MaxPooling2D(pool_size=2,2))
-model.add(Dropout(0.25))
+model.add(Conv2D(6,(5,5), activation = 'relu'))
+model.add(MaxPooling2D())
+model.add(Conv2D(6,(5,5), activation = 'relu'))
+model.add(MaxPooling2D())
 model.add(Flatten())
 model.add(Dense(120))
+model.add(Dropout(0.5))
 model.add(Dense(84))
 model.add(Dense(1))
 model.compile(loss= 'mse', optimizer = 'adam')
-model.fit(X_train, y_train, validation_split= 0.3, shuffle= True, epochs= 4)
+model.fit(X_train, y_train, validation_split = 0.3, shuffle = True, epochs = 4, verbose = 2)
 model.save('model.h5')
